da4f6c37a4ba8ea4b5f7a2db66b2a00c5ca7b32a
D4rkPh0en1x <frank@datateam.lu>
2018-03-11 12:23:23 +0100

    Update

---
+++ src/Controller/login.php
@@ -0,0 +1,5 @@
+<form class="loginForm" method="post">
+  <input type="mail" name="mail" value="">
+  <input type="password" name="password" value="">
+  <input type="button" name="send" value="">
+</form>
