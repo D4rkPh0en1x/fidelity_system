7be773e4b59d26b5ae934228157554bf3d8cf278
D4rkPh0en1x <frank@datateam.lu>
2018-03-18 15:14:30 +0100

    Changed different files to get hashed password to work

--- src/Controller/login.php
+++ src/Controller/login.php
@@ -3,14 +3,15 @@ $email = $password = "";
 $email_err = $password_err = "";
 if($_SERVER["REQUEST_METHOD"] == "POST"){
   if(empty(trim($_POST["email"]))){
-      $email_err = 'Please enter your email address';
+      $email_err = 'Please enter email.';
   } else{
       $email = trim($_POST["email"]);
   }
   if(empty(trim($_POST['password']))){
-      $password_err = 'Please enter your password';
+      $password_err = 'Please enter your password.';
   } else{
       $password = trim($_POST['password']);
+      $hashed_password = hash('sha512', $password);
   }
   if(empty($email_err) && empty($password_err)){
     $pdo = Service\DBConnector::getConnection();
@@ -22,7 +23,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         if($stmt->rowCount() == 1){
           if($row = $stmt->fetch()){
             $db_password = $row['password'];
-            if($password === $db_password){
+            if($hashed_password === $db_password){
               session_start();
               $_SESSION['email'] = $email;
               header("location: /index.php/account");
@@ -43,14 +44,22 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 <h2>Please login to your account</h2>
 <div class="container">
   <div class="card card-container">
-      <form class="form-signin">
-          <span id="reauth-email" class="reauth-email"></span>
-          <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-          <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+      <form method="post" class="form-signin">
+        <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+          <input type="email" name="email" id="inputEmail" class="form-control" value="<?php echo htmlentities($_POST['email'] ?? '') ?>" placeholder="Email address" required autofocus>
+          <span class="help-block"><?php echo $email_err; ?></span>
+        </div>
+        <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+          <input type="password" name="password" id="inputPassword" class="form-control" value="<?php echo htmlentities($_POST['password'] ?? '') ?>" placeholder="Password" required>
           <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Sign in</button>
+          <span class="help-block"><?php echo $password_err; ?></span>
+        </div>
       </form>
       <a href="/index.php/register" class="register-account">
           Register account
+      </a>
+      <a href="/index.php/account" class="register-account">
+          Login done? -> Account
       </a>
   </div>
 </div>
--- src/Controller/register.php
+++ src/Controller/register.php
@@ -1,49 +1,74 @@
+<?php
+try{
+  $connection = Service\DBConnector::getConnection();
+} catch (\PDOException $exception) {
+  http_response_code(500);
+  echo 'Impossible to connect to the DB, contact the support';
+  exit(10);
+}
+$sqlcountry = 'SELECT * FROM countries';
+$statement = $connection->prepare($sqlcountry);
+$statement->execute();
+$allcountries = $statement->fetchall();
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Fidelity System - Register</title>
-
-</head>
-
-<body>
+if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+  $email = $_POST['email'] ?? null;
+  $password = $_POST['password'] ?? null;
+  $hashed_password = hash('sha512', $_POST['password']) ?? null;
+  $firstname = $_POST['firstname'] ?? null;
+  $lastname = $_POST['lastname'] ?? null;
+  $phonenumber = $_POST['phonenumber'] ?? null;
+  $phonenumber = intval($phonenumber);
+  $street = $_POST['street'] ?? null;
+  $streetnumber = $_POST['streetnumber'] ?? null;
+  $streetnumber = intval($streetnumber);
+  $postalcode = $_POST['postalcode'] ?? null;
+  $postalcode = intval($postalcode);
+  $locality = $_POST['locality'] ?? null;
+  $country = $_POST['country'] ?? null;
+  $country = intval($country);
+  $region = $_POST['region'] ?? null;
+  $sql = "INSERT INTO user(email,password,firstname,lastname,phonenumber,street,streetnumber,postalcode,locality,region,country) VALUES (\"$email\", \"$hashed_password\", \"$firstname\", \"$lastname\", \"$phonenumber\", \"$street\", \"$streetnumber\", \"$postalcode\", \"$locality\", \"$region\", \"$country\" )";
+  $affected = $connection->exec($sql);
+  if (!$affected){
+    echo implode(', ', $connection->errorInfo());
+    return;
+  }
+  $_SESSION['email'] = $email;
+  header("location: /index.php/login");
+  return;
+}
+?>
 <h2>Please register your account</h2>
 <h4>The account will be set free by an admin after checking the provided data</h4>
-  <div class="container">
-          <div class="card cardregister-container">
-
-              <form class="form-register">
-                  <div class="register-left">
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required>
-                    <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-                    <input type="text" id="firstname" class="form-control" name="firstname" placeholder="firstname" value="">
-                    <input type="text" id="street" class="form-control" name="street" placeholder="street" value="">
-                  </div>
-                  <div class="register-right">
-                    <input type="email" id="verifyinputEmail" class="form-control" placeholder="Verify email address" required>
-                    <input type="password" id="verifyinputPassword" class="form-control" placeholder="Verify password" required>
-                    <input type="text" id="lastname" class="form-control" name="lastname" placeholder="lastname" value="">
-                    <input type="text" id="number" class="form-control" name="number" placeholder="number" value="">
-                    <button class="btn btn-lg btn-primary btn-block btn-signin" type="submit">Register account</button>
-                  </div>
-                  <!--<div id="remember" class="checkbox">
-                      <label>
-                          <input type="checkbox" value="remember-me"> Remember me
-                      </label>
-                  </div>-->
-
-              </form>
-              <a href="/" class="login-account">
-                  Login to account
-              </a>
-              <!--<a href="#" class="forgot-password">
-                  Forgot the password?
-              </a>-->
-          </div>
+<div class="container container-register">
+  <div class="card cardregister-container">
+    <form method="POST" class="form-register">
+      <div class="register-left">
+        <input type="email" name="email" id="email" class="form-control" value="<?php echo htmlentities($_POST['email'] ?? '') ?>" placeholder="Email address" required autofocus>
+			  <input type="text" id="firstname" name="firstname" class="form-control"  placeholder="First Name" required value="<?php echo htmlentities($_POST['firstname'] ?? '') ?>"/>
+        <input type="text" id="street" name="street" class="form-control" value="<?php echo htmlentities($_POST['street'] ?? '') ?>" placeholder="Street Name" required>
+        <input type="number" id="postalcode" name="postalcode" class="form-control" value="<?php echo htmlentities($_POST['postalcode'] ?? '') ?>" placeholder="Postal Code" required>
+          <?php
+          echo '<select id="country" name="country" class="form-control" placeholder="Country">';
+          foreach ($allcountries as $row){
+          echo "<option value='" . $row['id'] . "'>" . $row['country_name'] . "</option>";
+          }
+          echo "</select>";
+          ?>
+        <input type="number" id="phonenumber" name="phonenumber" class="form-control" value="<?php echo htmlentities($_POST['phonenumber'] ?? '') ?>" placeholder="Phone Number" required>
       </div>
-
-    </body>
-</html>
+      <div class="register-right">
+        <input type="password" name="password" id="password" class="form-control" placeholder="Password" required>
+        <input type="text" id="lastname" name="lastname" class="form-control"  placeholder="Last Name" required value="<?php echo htmlentities($_POST['lastname'] ?? '') ?>"/>
+        <input type="number" id="streetnumber" name="streetnumber" class="form-control" value="<?php echo htmlentities($_POST['streetnumber'] ?? '') ?>" placeholder="House Number" required>
+        <input type="text" id="locality" name="locality" class="form-control" value="<?php echo htmlentities($_POST['locality'] ?? '') ?>" placeholder="Locality" required>
+        <input type="text" id="region" name="region" class="form-control" value="<?php echo htmlentities($_POST['region'] ?? '') ?>" placeholder="Region - if exists">
+        <br /><br /><button class="btn btn-lg btn-primary btn-block btn-signin btn-register" type="submit">Register</button>
+      </div>
+    </form>
+    <a href="/index.php/login" class="login-account">
+    Login to your account
+    </a>
+  </div>
+</div>
